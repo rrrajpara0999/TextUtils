670e1ae1fe0b04e0ba8e2835aa65b58c57a914fa
rrrajpara0999 <137982183+rrrajpara0999@users.noreply.github.com>
2023-06-28 12:54:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './Appa.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+  
+<Navbar title="TextUtils2" aboutText="aboutaaatext Utils" />
+{/* <Navbar/> */}
+   </>
+  );
+}
+
+export default App;
